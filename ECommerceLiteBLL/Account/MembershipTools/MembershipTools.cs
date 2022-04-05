fc98af35b6b3b464bf78c26a338dacf55b29749c
Enes Çetintaş <enesctnts@gmail.com>
2022-04-05 10:24:00 +0300

    RepositoryBase oluşturuldu.

---
+++ ECommerceLiteBLL/Account/MembershipTools/MembershipTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceLiteBLL.Account.MembershipTools
+{
+    public static class MembershipTools
+    {
+
+    }
+}
