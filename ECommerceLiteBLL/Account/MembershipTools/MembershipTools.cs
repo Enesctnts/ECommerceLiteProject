91e3d43230e04007075568738a4313e924732e0c
Enes Çetintaş <enesctnts@gmail.com>
2022-04-05 10:49:47 +0300

    BLL Katamanına Membership Tools eklendi.

--- ECommerceLiteBLL/Account/MembershipTools/MembershipTools.cs
+++ ECommerceLiteBLL/Account/MembershipTools/MembershipTools.cs
@@ -1,13 +1,88 @@
-﻿using System;
+﻿using ECommerceLiteDAL;
+using ECommerceLiteEntity.IdentityModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ECommerceLiteBLL.Account.MembershipTools
 {
     public static class MembershipTools
     {
+        public static UserStore<ApplicationUser> NewUserStore()
+        {
+            return new UserStore<ApplicationUser>(new MyContext());
+        }
+
+        public static UserManager<ApplicationUser> NewUserManager()
+        {
+            return new UserManager<ApplicationUser>(NewUserStore());
+        }
+
+        public static RoleStore<ApplicationRole> NewRoleStore()
+        {
+            return new RoleStore<ApplicationRole>(new MyContext());
+        }
+
+        public static RoleManager<ApplicationRole> NewRoleManager()
+        {
+            return new RoleManager<ApplicationRole>(NewRoleStore());
+        }
+    
+        public static string GetUserName(string id)
+        {
+            var usermanager = NewUserManager();
+            var user = usermanager.FindById(id);
+
+            if (user!= null)
+            {
+                return user.UserName;
+
+            }
+            return null;
+        }
+
+        public static string GetNameSurname()
+        {
+            var id = HttpContext.Current.User.Identity.GetUserId();
+
+            if (string.IsNullOrEmpty(id))
+            {
+                return "Ziyaretçi";
+
+            }
+            var usermanager = NewUserManager();
+            var user = usermanager.FindById(id);
+            string usernamesurname = string.Empty;
+            //string usernamesurname = ""; // aynı şey
+            usernamesurname = user != null ?
+                //string.Format("{0} {1}" , user.Name , user.Surname) //alttakiyle aynı şey
+                //user.Name + " " + user.Surname // alttakiyle aynı şey 
+                $"{user.Name} {user.Surname}"
+                :
+                "Ziyaretiçi";
+
+            return usernamesurname;
+        }
+
+        public static ApplicationUser GetUser()
+        {
+            var id = HttpContext.Current.User.Identity.GetUserId();
+
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+            var myusermanager = NewUserManager();
+            var user = myusermanager.FindById(id);
+            return user;
+        }
+
+
 
     }
 }
