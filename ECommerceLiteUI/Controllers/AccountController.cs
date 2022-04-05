de4467b58cc798d78434343b3c466816c4101d0e
Enes Çetintaş <enesctnts@gmail.com>
2022-04-05 12:32:49 +0300

    Controller eklendi.

---
+++ ECommerceLiteUI/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceLiteBLL.Account;
+using ECommerceLiteBLL.Account.MembershipTools;
+using ECommerceLiteBLL.Repository;
+using ECommerceLiteBLL.Settings;
+using ECommerceLiteEntity.IdentityModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class AccountController : BaseController
+    {
+
+        //Global alan
+        //not: Bir sonraki projede repoları newlemeyeceğiz !
+        //çünkü bu bağımlılık oluşturur. Bir sonraki projede bağımlılıkları tersinne çevirme işlemi olarak bilinen dependency ınjection işlemleri yapacağız.
+
+        CustomerRepo myCustomerRepo = new CustomerRepo();
+        PassiveUserRepo myPassiveUserRepo = new PassiveUserRepo();
+        UserManager<ApplicationUser> myUserManager = MembershipTools.NewUserManager();
+        UserStore<ApplicationUser> myUserStore = MembershipTools.NewUserStore();
+        RoleManager<ApplicationRole> myURoleManager = MembershipTools.NewRoleManager();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
