f11602181bf45d95c3b742beb37db00a5a6be605
Enes Çetintaş <enesctnts@gmail.com>
2022-04-14 11:09:06 +0300

    Veritabanında ProductPicture tablosu oluşturuldu.

---
+++ ECommerceLiteDAL/Migrations/202204140649536_add_pictures.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceLiteDAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_pictures : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Tables", newName: "ProductPictures");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.ProductPictures", newName: "Tables");
+        }
+    }
+}
