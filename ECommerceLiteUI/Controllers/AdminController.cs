4de23692f5abe19a7bfae78dda14563a64618f7e
Enes Çetintaş <enesctnts@gmail.com>
2022-04-18 15:07:58 +0300

    Dashboard ve Orders metodları eklendi.

--- ECommerceLiteUI/Controllers/AdminController.cs
+++ ECommerceLiteUI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceLiteBLL.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +7,22 @@ using System.Web.Mvc;
 
 namespace ECommerceLiteUI.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
-       
+        OrderRepo myOderRepo = new OrderRepo();
+
+        public ActionResult Dashboard()
+        {
+            //Bu ay oluşturulan sipariş sayısını ekranda görelim
+            var orderlist = myOderRepo.GetAll();
+            var CurrentMontlyOrderCount = orderlist.Where(x => x.RegisterDate >= DateTime.Now.AddMonths(-1)).ToList().Count;
+            ViewBag.NewOrderCount = CurrentMontlyOrderCount;
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
     }
 }
