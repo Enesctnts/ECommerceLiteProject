88ba5599ec239f923e2320cd51168833358c5de6
Enes Çetintaş <enesctnts@gmail.com>
2022-04-18 15:05:16 +0300

    Glabal asax içinde default admin kullanıcı oluşturuldu.

--- ECommerceLiteUI/Global.asax.cs
+++ ECommerceLiteUI/Global.asax.cs
@@ -9,6 +9,9 @@ using ECommerceLiteBLL.Account.MembershipTools;
 using ECommerceLiteEntity.Enums;
 using Microsoft.AspNet.Identity;
 using ECommerceLiteEntity.IdentityModels;
+using ECommerceLiteEntity.Models;
+using ECommerceLiteBLL.Repository;
+
 namespace ECommerceLiteUI
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -19,31 +22,75 @@ namespace ECommerceLiteUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //Application_Start: Uygulama ilk kez çalıştıgında bir defaya mahsus olmak üzere çalışır.
-            //Bu nedenle ben uygulama ilk kez çalıştıgında DB'de Roller ekli mi diye bakmak istiyorum.
-            //Ekli değilse rolleri Enum'dan çagırıp ekleyelim.Ekli ise bişey yapmaya gerek kalmıyor.
 
-            //adım1: Rollere bakıcagım şey --> Role Manager
+            #region CreateRoles_RolleriOlustur
+            //NOT: Application_Start :
+            //Uygulama ilk kez çalıştırıldığında bir defaya mahsus olmak üzere çalışır.
+            //Bu nedenle ben uyg. ilk kez çalıştığında DB'de Roller ekli mi diye bakmak istiyorum.
+            //Ekli değilse rolleri Enum'dan çağırıp ekleyelim
+            //Ekli ise bişey yapmaya gerek kalmıyor.
+
+            //adım 1: Rollere bakacağım şey --> Role Manager
             var myRoleManager = MembershipTools.NewRoleManager();
-            //Adım2:Rollerin isimlerini almak için (ipucu-->Enum)
+            //adım 2: Rollerin isimlerini almak (ipucu --> Enum)
             var allRoles = Enum.GetNames(typeof(Roles));
-            //Bize gelen diziyi tek tek döneceğiz(Döngü)
+            //adım 3: Bize gelen diziyi tek tek tek döneceğiz (döngü)
+
             foreach (var item in allRoles)
             {
-                //adım4: Acaba b rol Db'de ekli mi?
-                if (!myRoleManager.RoleExists(item))//Eğer bu role ekli değilse ?
+                //adım 4: Acaba bu rol DB'de ekli mi? 
+                if (!myRoleManager.RoleExists(item)) // Eğer bu role ekli değilse?
                 {
-                    //Adım5: Rolü ekle!
-                    myRoleManager.Create(new ApplicationRole()
+                    //Adım 5: Rolü ekle!
+                    //1.yol
+                    ApplicationRole role = new ApplicationRole()
                     {
                         Name = item,
                         IsDeleted = false
+                    };
+                    myRoleManager.Create(role);
+                    //2.yol
+                    //myRoleManager.Create(new ApplicationRole()
+                    //{
+                    //    Name = item
+                    //});
 
-                    });
                 }
             }
 
-            
+            #endregion CreateDefaultAdmin_SistemAdminiOlustur
+            //NOT: Proje ilk ayağa kalkığında arka planda default admin kullanıcısı ekeleylim
+            //NOT: Kendi isminizle admin olarak kayıt olmanız için Admin register sayfası zaman kısıtlılığından yapamadık. Geniş bir zamanda eklenebilir.
+            var myUserManager = MembershipTools.NewUserManager();
+            var allUsers = myUserManager.Users;
+            AdminRepo myAdminRepo = new AdminRepo();
+            if (myAdminRepo.GetAll().Count==0)
+            {
+                ApplicationUser adminUser = new ApplicationUser()
+                {
+                    Name = "303",
+                    Surname = "Admin",
+                    RegisterDate = DateTime.Now,
+                    Email = "nayazilim303@gmail.com",
+                    UserName = "nayazilim303@gmail.com",
+                    IsDeleted = false,
+                    EmailConfirmed = true
+                };
+                var createResult = myUserManager.Create(adminUser, "admin12345");
+                if (createResult.Succeeded)
+                {
+                    myUserManager.AddToRole(adminUser.Id, Roles.Admin.ToString());
+                    Admin admin = new Admin()
+                    {
+                        UserId=adminUser.Id,
+                        TCNumber="00000000000",
+                        IsDeleted=false,
+                        LastActiveTime=DateTime.Now
+                    };
+                    myAdminRepo.Insert(admin);
+                }
+            }
+
         }
         protected void Application_Error()
         {
