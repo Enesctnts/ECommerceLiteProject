16323aea58dc630f6bef6284e28159f60735fb4a
Enes Çetintaş <enesctnts@gmail.com>
2022-04-20 12:45:39 +0300

    Sipariş için sipariş sepeti oluşturuldu.

--- ECommerceLiteUI/Controllers/HomeController.cs
+++ ECommerceLiteUI/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace ECommerceLiteUI.Controllers
             //Ana kategorileri viewbag ile sayfaya gönderelim
             var categoryList = myCategoryRepo.AsQueryable().Where(x => x.BaseCategoryId == null).Take(4).ToList();
 
-            ViewBag.CategoryList = categoryList.OrderByDescending(x => x.Id);
+            ViewBag.CategoryList = categoryList.OrderByDescending(x => x.Id).ToList();
 
             //ürünler 
             var productList = myProductRepo.AsQueryable().Where(x => x.IsDeleted == false && x.Quantity >= 1).Take(10).ToList();
@@ -62,5 +62,75 @@ namespace ECommerceLiteUI.Controllers
 
             return View();
         }
+
+        public ActionResult AddToCart(int id)
+        {
+            try
+            {
+                //Session'a eklenecek
+                //Session oturum demektir
+                var shoppingCart = Session["ShoppingCart"] as List<ProductViewModel>;
+                if (shoppingCart==null)
+                {
+                    shoppingCart = new List<ProductViewModel>();
+                }
+                if (0<id)
+                {
+                    var product = myProductRepo.GetById(id);
+                    if (product==null)
+                    {
+                        TempData["AddToCartFailed"] = "Ürün eklemesi başarısızdır. Lütfen tekrar deneyiniz";
+                        //Product null geldi logla
+                        return RedirectToAction("Index", "Home");
+                    }
+                    //Tamam ekleme yapılacak 
+                    //ProductViewModel productAddToCart = product.Adapt<ProductViewModel>();
+
+                    var productAddToCart = new ProductViewModel()
+                    {
+                        Id = product.Id,
+                        ProductName = product.ProductName,
+                        Description = product.Description,
+                        Discount = product.Discount,
+                        Price = product.Price,
+                        Quantity = product.Quantity,
+                        CategoryId = product.CategoryId,
+                        RegisterDate = product.RegisterDate,
+                        ProductCode = product.ProductCode
+                    };
+                    //Aynı üründen varsa miktar artırılacak
+                    if (shoppingCart.Count(x=>x.Id== productAddToCart.Id) >0)
+                    {
+                        shoppingCart.FirstOrDefault(x => x.Id == productAddToCart.Id).Quantity++;
+
+                    }
+                    else
+                    {
+                        //Aynı üründen yoksa miktar 1 olucak ve sepete ekledik.
+                        productAddToCart.Quantity = 1;
+                        shoppingCart.Add(productAddToCart);
+                    }
+                    //Önemli --> Session'a bu listeyi atamamız gerekli
+                    Session["ShoppingCart"] = shoppingCart;
+                    TempData["AddToCardSuccess"] = "Ürün sepete eklendi";
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    TempData["AddToCartFailed"] = "Ürün eklemesi başarısızdır. Lütfen tekrar deneyiniz";
+                    //Loglama yap id düzgün gelmedi
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                //ex loglanacak
+                TempData["AddToCartFailed"] = "Ürün eklemesi başarısızdır. Lütfen tekrar deneyiniz";
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
     }
 }
