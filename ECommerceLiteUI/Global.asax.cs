a7ca61808bb295f2939b94a349a5862a2fc9ae4a
Enes Çetintaş <enesctnts@gmail.com>
2022-04-22 15:15:24 +0300

    Düzenlemeler yapıldı

--- ECommerceLiteUI/Global.asax.cs
+++ ECommerceLiteUI/Global.asax.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity;
 using ECommerceLiteEntity.IdentityModels;
 using ECommerceLiteEntity.Models;
 using ECommerceLiteBLL.Repository;
+using ECommerceLiteUI.LogManaging;
 
 namespace ECommerceLiteUI
 {
@@ -23,83 +24,112 @@ namespace ECommerceLiteUI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            #region CreateRoles_RolleriOlustur
-            //NOT: Application_Start :
-            //Uygulama ilk kez çalıştırıldığında bir defaya mahsus olmak üzere çalışır.
-            //Bu nedenle ben uyg. ilk kez çalıştığında DB'de Roller ekli mi diye bakmak istiyorum.
-            //Ekli değilse rolleri Enum'dan çağırıp ekleyelim
-            //Ekli ise bişey yapmaya gerek kalmıyor.
-
-            //adım 1: Rollere bakacağım şey --> Role Manager
-            var myRoleManager = MembershipTools.NewRoleManager();
-            //adım 2: Rollerin isimlerini almak (ipucu --> Enum)
-            var allRoles = Enum.GetNames(typeof(Roles));
-            //adım 3: Bize gelen diziyi tek tek tek döneceğiz (döngü)
-
-            foreach (var item in allRoles)
+            try
             {
-                //adım 4: Acaba bu rol DB'de ekli mi? 
-                if (!myRoleManager.RoleExists(item)) // Eğer bu role ekli değilse?
+                Logger.LogMessage("**********APLICATION STARTED*********");
+                #region CreateRoles_RolleriOlustur
+                //NOT: Application_Start :
+                //Uygulama ilk kez çalıştırıldığında bir defaya mahsus olmak üzere çalışır.
+                //Bu nedenle ben uyg. ilk kez çalıştığında DB'de Roller ekli mi diye bakmak istiyorum.
+                //Ekli değilse rolleri Enum'dan çağırıp ekleyelim
+                //Ekli ise bişey yapmaya gerek kalmıyor.
+
+                //adım 1: Rollere bakacağım şey --> Role Manager
+                var myRoleManager = MembershipTools.NewRoleManager();
+                //adım 2: Rollerin isimlerini almak (ipucu --> Enum)
+                var allRoles = Enum.GetNames(typeof(Roles));
+                //adım 3: Bize gelen diziyi tek tek tek döneceğiz (döngü)
+                foreach (var item in allRoles)
                 {
-                    //Adım 5: Rolü ekle!
-                    //1.yol
-                    ApplicationRole role = new ApplicationRole()
+                    //adım 4: Acaba bu rol DB'de ekli mi? 
+                    if (!myRoleManager.RoleExists(item)) // Eğer bu role ekli değilse?
                     {
-                        Name = item,
-                        IsDeleted = false
+                        //Adım 5: Rolü ekle!
+                        //1.yol
+                        ApplicationRole role = new ApplicationRole()
+                        {
+                            Name = item,
+                            IsDeleted = false
+                        };
+                        myRoleManager.Create(role);
+                        //2.yol
+                        //myRoleManager.Create(new ApplicationRole()
+                        //{
+                        //    Name = item
+                        //});
+
+                    }
+                }
+
+                #endregion
+
+                #region CreateDefaultAdmin_SistemAdminiOlustur
+                //NOT: Proje ilk ayağa kalkığında arka planda default admin kullanıcısı ekeleylim
+                //NOT: Kendi isminizle admin olarak kayıt olmanız için Admin register sayfası zaman kısıtlılığından yapamadık. Geniş bir zamanda eklenebilir.
+
+                var myUserManager = MembershipTools.NewUserManager();
+                var allUsers = myUserManager.Users;
+                AdminRepo myAdminRepo = new AdminRepo();
+                if (myAdminRepo.GetAll().Count == 0) // Hiç admin yoksa ekleyelim
+                {
+                    ApplicationUser adminUser = new ApplicationUser()
+                    {
+                        Name = "303",
+                        Surname = "Admin",
+                        RegisterDate = DateTime.Now,
+                        Email = "nayazilim303@gmail.com",
+                        UserName = "nayazilim303@gmail.com",
+                        IsDeleted = false,
+                        EmailConfirmed = true
                     };
-                    myRoleManager.Create(role);
-                    //2.yol
-                    //myRoleManager.Create(new ApplicationRole()
-                    //{
-                    //    Name = item
-                    //});
+                    var createResult = myUserManager.Create(adminUser, "admin12345");
+                    if (createResult.Succeeded)
+                    {
+                        myUserManager.AddToRole(adminUser.Id, Roles.Admin.ToString());
+                        Admin admin = new Admin()
+                        {
+                            UserId = adminUser.Id,
+                            TCNumber = "00000000000",
+                            IsDeleted = false,
+                            LastActiveTime = DateTime.Now
+                        };
+                        myAdminRepo.Insert(admin);
+                    }
 
                 }
+
+
+                #endregion
             }
-
-            #endregion CreateDefaultAdmin_SistemAdminiOlustur
-            //NOT: Proje ilk ayağa kalkığında arka planda default admin kullanıcısı ekeleylim
-            //NOT: Kendi isminizle admin olarak kayıt olmanız için Admin register sayfası zaman kısıtlılığından yapamadık. Geniş bir zamanda eklenebilir.
-            var myUserManager = MembershipTools.NewUserManager();
-            var allUsers = myUserManager.Users;
-            AdminRepo myAdminRepo = new AdminRepo();
-            if (myAdminRepo.GetAll().Count==0)
+            catch (Exception ex)
             {
-                ApplicationUser adminUser = new ApplicationUser()
-                {
-                    Name = "303",
-                    Surname = "Admin",
-                    RegisterDate = DateTime.Now,
-                    Email = "nayazilim303@gmail.com",
-                    UserName = "nayazilim303@gmail.com",
-                    IsDeleted = false,
-                    EmailConfirmed = true
-                };
-                var createResult = myUserManager.Create(adminUser, "admin12345");
-                if (createResult.Succeeded)
-                {
-                    myUserManager.AddToRole(adminUser.Id, Roles.Admin.ToString());
-                    Admin admin = new Admin()
-                    {
-                        UserId=adminUser.Id,
-                        TCNumber="00000000000",
-                        IsDeleted=false,
-                        LastActiveTime=DateTime.Now
-                    };
-                    myAdminRepo.Insert(admin);
-                }
+
+                Logger.LogMessage(ex.ToString(), "Application_Start", "system");
             }
 
         }
         protected void Application_Error()
         {
-            //Not: İhtiyacım olursa internetten Global.asax'ın metodlarına bakıp kullanabilirim
-            //Örn: Application_Error: Uygulama içinde istenmeyen bir hata meydana geldiğinde çalışır.
-            //Bu metodu yazarsak o hatayı loglayıp sorunu çözebilirsiniz.
+            //NOT: ihtiyacım olursa internetten Global.asax'ın metotlarına bakıp kullanabilirim
+            //ÖRN: Application_Error : Uygulama içinde istenmeyen bir hata meydana geldiğinde çalışır. Bu metodu yazarsak o hatayı loglayıp sorunu çözebiliriz.
 
-            Exception exception = Server.GetLastError();
-            //ex loglanacak
+            Exception ex = Server.GetLastError();
+            Logger.LogMessage(ex.ToString(), "Application_Error");
+        }
+
+        protected void Application_End()
+        {
+            Logger.LogMessage("**********APLICATION FINISHED*********");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Logger.LogMessage("Session Started" + sender.ToString());
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Logger.LogMessage("Session Ended" + sender.ToString());
         }
     }
 }
