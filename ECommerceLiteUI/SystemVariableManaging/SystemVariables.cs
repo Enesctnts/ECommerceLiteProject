ed25d9d088deddef95daaf88eaeb546bc38adfcf
Enes Çetintaş <enesctnts@gmail.com>
2022-04-25 11:46:54 +0300

    Utilities eklendi.Webconfig içindeki appsettings içine eklenen bilgilere System ConfigurationManager ile nasıl erişebileceğimiz işlendi.

---
+++ ECommerceLiteUI/SystemVariableManaging/SystemVariables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+namespace ECommerceLiteUI.SystemVariableManaging
+{
+    public static class SystemVariables
+    {
+        public static string EMAIL
+        {
+            get
+            {
+                try
+                {
+                    //return System.Configuration.ConfigurationManager.AppSettings["ECommerceLiteEmail"].ToString();
+                    return ConfigurationManager.AppSettings["ECommerceLiteEmail"].ToString();
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("Webconfig dosyasında email bilgisi bulunamadı!");
+                }
+            }
+        }
+    }
+} 
