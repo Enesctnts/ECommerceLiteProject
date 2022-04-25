577bcf9cce2d7144fcdccae02f849f45eaaabe4b
Enes Çetintaş <enesctnts@gmail.com>
2022-04-25 12:10:58 +0300

    ForeignKey hatası çözmeye çalışıyorum.

---
+++ ECommerceLiteDAL/Migrations/202204250858177_ForeignKey.cs
@@ -0,0 +1,20 @@
+﻿namespace ECommerceLiteDAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ForeignKey : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String(maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String(nullable: false, maxLength: 500));
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(nullable: false, maxLength: 100));
+        }
+    }
+}
